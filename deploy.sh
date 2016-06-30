1c52fe4709a5e39db4a4eec59dfccf64b2794d5f
Xemiru Sanzenin <xemiruk@gmail.com>
2016-06-29 17:41:28 -0700

    Clean the output folder during builds.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-	mkdocs build -d out
+	mkdocs build -c -d out
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
